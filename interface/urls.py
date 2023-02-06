6288017a461d94aa4be5cf7f3c43cc6bf476fb35
aishatasaduq <aishatasaduq@github.com>
2023-02-06 07:15:26 +0000

    changes

--- interface/urls.py
+++ interface/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import include, path
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
 
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("password_reset/", include('django_rest_passwordreset.urls', namespace='password_reset'))
 ]
--- project/urls.py
+++ project/urls.py
@@ -19,5 +19,4 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(("interface.urls", "interface"), namespace="interface")),
-    path("api/password_reset/", include('django_rest_passwordreset.urls', namespace='password_reset'))
 ]
