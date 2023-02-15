2fba77b1b0c2c973b0f601d2436e9dd3ca731bad
aishatasaduq <aishatasaduq@github.com>
2023-02-15 10:20:01 +0000

    changes

--- interface/urls.py
+++ interface/urls.py
@@ -4,7 +4,6 @@ from rest_framework_simplejwt.views import (TokenObtainPairView,
 from django.urls import reverse_lazy
 from django.views.generic import RedirectView
 
-app_name = 'interface'
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
