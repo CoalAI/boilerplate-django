8a9cbc8810e82f4ddf3c4442434f12f59e316441
aishatasaduq <aishatasaduq@github.com>
2023-09-18 11:16:40 +0000

    update and retrieve user profile

--- interface/urls.py
+++ interface/urls.py
@@ -3,10 +3,12 @@ from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
 from django.urls import reverse_lazy
 from django.views.generic import RedirectView
+from .views import UserProfileUpdateView
 
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path('accounts/', include('authemail.urls')),
-    path('social-auth/', RedirectView.as_view(url=reverse_lazy('social:begin', args=['google-oauth2'])), name='social-auth')
+    path('social-auth/', RedirectView.as_view(url=reverse_lazy('social:begin', args=['google-oauth2'])), name='social-auth'),
+    path('update-profile/', UserProfileUpdateView.as_view())
 ]
