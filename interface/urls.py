a8810a69a705351cf6df947f37617cb260b2b903
aishatasaduq <aishatasaduq@github.com>
2023-09-20 06:29:10 +0000

    added signup url

--- interface/urls.py
+++ interface/urls.py
@@ -3,10 +3,11 @@ from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
 from django.urls import reverse_lazy
 from django.views.generic import RedirectView
+from authemail.views import Signup
 
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path('accounts/', include('authemail.urls')),
+    path('signup/', Signup.as_view(), name="signup"),
     path('social-auth/', RedirectView.as_view(url=reverse_lazy('social:begin', args=['google-oauth2'])), name='social-auth')
 ]
