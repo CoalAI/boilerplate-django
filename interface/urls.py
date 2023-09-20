c09935ffd0eaa4dbb8409b1614ce761bd4078eda
aishatasaduq <aishatasaduq@github.com>
2023-09-20 07:24:25 +0000

    password change url added

--- interface/urls.py
+++ interface/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
 from django.urls import reverse_lazy
 from django.views.generic import RedirectView
 from authemail.views import (PasswordReset, PasswordResetVerified,
-                              SignupVerify)
+                              SignupVerify, PasswordChange)
 
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
@@ -12,5 +12,6 @@ urlpatterns = [
     path('signup/verify/', SignupVerify.as_view(), name='signup-verify'),
     path('password/reset/', PasswordReset.as_view(), name='password-reset'),
     path('password/reset/verified/', PasswordResetVerified.as_view(), name='password-reset-verified'),
+    path('password/change/', PasswordChange.as_view(), name='password-change'),
     path('social-auth/', RedirectView.as_view(url=reverse_lazy('social:begin', args=['google-oauth2'])), name='social-auth')
 ]
